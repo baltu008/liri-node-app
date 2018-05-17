969a0fdc8111363de4ff5cb2e6e439a98bb3790f
Eric Baltutis <baltu008@umn.edu>
2018-05-17 16:12:54 -0500

    add keys and liri.js

--- keys.js
+++ keys.js
@@ -1,14 +1,15 @@
-js
+```js
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.C27HdX1blWhNdpqT9gVcdQn9F,
+  consumer_secret: process.env.gUvr1YIMzknCsBgEXGIarLh3XKTgPiF3IQjMNkZOypyaW0xBSD,
+  access_token_key: process.env.996529275544244224-HSafVeHsBdeFpeIx72TOJ1s0ER6i1q4,
+  access_token_secret: process.env.	ICAc44Tb4P5JF0q5WxVhSYQ3XPq8QqjtmcnZlnoAr7ogl
 };
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.1b6a4b7479e6442fbf26ef221110f4a0,
+  secret: process.env.3c3d2674d20e4faa81003060d2d30020
 };
+```
---
+++ liri.js
@@ -0,0 +1,11 @@
+js
+require("dotenv").config();
+
+js
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+`my-tweets`
+`spotify-this-song`
+`movie-this`
+`do-what-it-says`
