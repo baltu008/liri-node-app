b565f85b945179e2f1435bb2865caa1d1bcd94ee
Eric Baltutis <baltu008@umn.edu>
2018-05-18 23:22:34 -0500

    more functions

--- keys.js
+++ keys.js
@@ -1,15 +1,11 @@
-```js
-console.log('this is loaded');
-
 exports.twitter = {
-  consumer_key: process.env.C27HdX1blWhNdpqT9gVcdQn9F,
-  consumer_secret: process.env.gUvr1YIMzknCsBgEXGIarLh3XKTgPiF3IQjMNkZOypyaW0xBSD,
-  access_token_key: process.env.996529275544244224-HSafVeHsBdeFpeIx72TOJ1s0ER6i1q4,
-  access_token_secret: process.env.	ICAc44Tb4P5JF0q5WxVhSYQ3XPq8QqjtmcnZlnoAr7ogl
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env.1b6a4b7479e6442fbf26ef221110f4a0,
-  secret: process.env.3c3d2674d20e4faa81003060d2d30020
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
-```
--- liri.js
+++ liri.js
@@ -1,11 +1,68 @@
-js
 require("dotenv").config();
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var keys = require('./keys');
+var request = require('request');
 
-js
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var argument = process.argv[2];
+var songArg = process.argv[3];
+var movieArg = process.argv[3];
 
-`my-tweets`
-`spotify-this-song`
-`movie-this`
-`do-what-it-says`
+switch (argument) {
+    case "my-tweets":
+        var client = new Twitter(keys.twitter);
+        var params = { screen_name: 'EccoSquatty' };
+        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+            if (!error) {
+                for (var i = 0; i < tweets.length; i++) {
+                    console.log(tweets[i].text);
+                    console.log(tweets[i].created_at);
+                }
+            }
+            console.log(error)
+        });
+        break;
+    case "spotify-this-song":
+        var spotify = new Spotify(keys.spotify);
+        spotify.search({ type: 'track', query: songArg }, function (error, data) {
+            if (error) {
+                return console.log('Error occurred: ' + error);
+            } else
+                console.log(toTitleCase(`
+                Song: ${songArg}`),
+                    (`
+                Artist: ${data.tracks.items[0].album.artists[0].name}
+                Album: ${data.tracks.items[0].album.name}
+                Preview: ${data.tracks.items[0].album.artists[0].external_urls.spotify}
+                `));
+        });
+        break;
+    case "movie-this":
+        request("https://www.omdbapi.com/?t=" + movieArg + "&y=&plot=short&apikey=trilogy", function (error, body) {
+            if (error) {
+                return console.log('error:', + error);
+            } else
+            console.log(body); // How do I break this apart?
+        });
+                // (`
+            // Title: ${movie}
+            // Release Year:
+            // IMDB Rating:
+            // RT Rating:
+            // Country:
+            // Language:
+            // Plot:
+            // Actors:
+            // `))
+        // );
+        break;
+    case "do-what-it-says":
+        break;
+}
+
+///Capitalize first letters of song argument on call-back
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
