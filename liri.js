547dd7944b541dcfc13a7d979305574959d7db77
Eric Baltutis <baltu008@umn.edu>
2018-05-21 20:34:41 -0500

    fix read.js function

--- liri.js
+++ liri.js
@@ -3,6 +3,8 @@ var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
 var keys = require('./keys');
 var request = require('request');
+// fs is a core Node package for reading and writing files
+var fs = require("fs");
 
 var argument = process.argv[2];
 var songArg = process.argv[3];
@@ -55,6 +57,29 @@ switch (argument) {
         });
         break;
     case "do-what-it-says":
+
+
+        // This block of code will read from the "movies.txt" file.
+        // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+        // The code will store the contents of the reading inside the variable "data"
+        fs.readFile("random.txt", "utf8", function (error, data) {
+
+            // If the code experiences any errors it will log the error to the console.
+            if (error) {
+                return console.log(error);
+            }
+
+            // We will then print the contents of data
+            console.log(data);
+
+            // Then split it by commas (to make it more readable)
+            var dataArr = data.split(",");
+
+            // We will then re-display the content as an array for later use.
+            console.log(dataArr);
+
+        });
+
         break;
 }
 
